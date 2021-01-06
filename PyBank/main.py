58440e53ebc85ffe6171641d9262212dc495cfa3
Vanessa Juliana <vanessaj@Vanessas-MacBook.local>
2021-01-06 22:51:19 +0800

    Committing

---
+++ PyBank/main.py
@@ -0,0 +1,31 @@
+# Create a script that analyses the records to calculate
+## total no. of months in the dataset 
+## net total amount of "Profit/Losses" over the entire period 
+## the average of the changes in "Profit/Losses" over the entire period
+## the greatest increase in profit (date and amount) over the entire period 
+## the greatest decrease in losses (date and amount) over the entire period 
+
+## EXAMPLE
+# Financial Analysis
+# ----------------------------
+# Total Months: 86
+# Total: $38382578
+# Average  Change: $-2315.12
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($-2196167)
+
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+# -------------------------------------------------------------------------
+
+# Import modules to read csv
+import os
+import csv
+
+# Join path to csv file 'budget_data'
+csvpath = os.path.join('Desktop', '..', 'Python-Challenge', 'PyBank', 'Resources' , 'budget_data.csv')
+
+# Read file
+with open(csvpath) as csvfile:
+    budget_data = csv.reader(csvfile, delimiter=',')
+    print(budget_data)
